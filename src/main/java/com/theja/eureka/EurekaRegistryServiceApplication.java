1a6b52d142347d85f7b9aea5961303fb26bca66c
unknown <k.ntr30@gmail.com>
2020-08-31 19:16:47 +0530

    Initial commit

---
+++ src/main/java/com/theja/eureka/EurekaRegistryServiceApplication.java
@@ -0,0 +1,37 @@
+package com.theja.eureka;
+
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaRegistryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaRegistryServiceApplication.class, args);
+	}
+
+}
+
+@RestController
+class ServiceInstanceRestController {
+
+	@Autowired
+	private DiscoveryClient discoveryClient;
+
+	@RequestMapping("/service-instances/{applicationName}")
+	public List<ServiceInstance> serviceInstancesByApplicationName(
+			@PathVariable String applicationName) {
+
+		return this.discoveryClient.getInstances(applicationName);
+	}
+}
---
+++ src/test/java/com/theja/eureka/EurekaRegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theja.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaRegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
